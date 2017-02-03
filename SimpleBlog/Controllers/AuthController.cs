f7fa5eda738bc04f460b3f310ecbb4d26aee4835
Gideon Gordon <Gideon Gordon>
2017-02-02 19:34:53 -0500

    Added basic login form
    View Models

--- SimpleBlog/Controllers/AuthController.cs
+++ SimpleBlog/Controllers/AuthController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿//Move data from a form to a controller
+
+using SimpleBlog.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +11,29 @@ namespace SimpleBlog.Controllers
 {
     public class AuthController : Controller
     {
+        //Get request go in to this method.
         public ActionResult Login()
         {
-            return View();
+            return View(new AuthLogin
+            { });
+        }
+
+
+        //Post request go into this method. 
+        [HttpPost] //httpPost - attribute to go in to the Post method. 
+        public ActionResult Login(AuthLogin form)
+        {
+
+            if (!ModelState.IsValid)
+                return View(form);
+
+            if (form.Username != "rainbow dash")
+            {
+                ModelState.AddModelError("Username", "Username or password isn't 20% cooler.");
+                return View(form);
+            }
+
+            return Content("The form is valid!");
         }
 
     }
---
+++ SimpleBlog/ViewModels/Auth.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimpleBlog.ViewModels
+{
+  public class AuthLogin
+    {
+        //Data going to be sent from view to controller
+        [Required]
+        public string Username { get; set; }
+
+        [Required, DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
